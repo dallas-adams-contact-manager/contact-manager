19d5827cc205076204bfad7ec9d183c71bf54ae0
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-09 22:30:25 -0600

    created print contacts method

--- src/ContactsApp.java
+++ src/ContactsApp.java
@@ -3,40 +3,37 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class ContactsApp {
 
     public static void openMenu() throws IOException {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Welcome to Contact Manager CLI");
         System.out.println();
         System.out.println("""
-                1. View Contacts
-                2. Add a new contact
-                3. Search a contact by name
-                4. Delete an existing contact
-                5. Exit
-                """);
-
+            1. View Contacts
+            2. Add a new contact
+            3. Search a contact by name
+            4. Delete an existing contact
+            5. Exit
+            """);
         int userChoice = scanner.nextInt();
-
         switch (userChoice) {
             case 1 -> viewContacts();
             case 2 -> addContacts();
-            case 3 -> searchContact();
-            case 4 -> deleteContact();
+//            case 3 -> searchContact();
+//            case 4 -> deleteContact();
             case 5 -> closeMenu();
             default -> openMenu();
         }
+
     }
 
+    public static void viewContacts() throws IOException {
+        printContacts();
 
-
+        openMenu();
+    }
 
     public static void addContacts() throws IOException {
         Scanner scanner = new Scanner(System.in);
@@ -64,20 +61,36 @@ public class ContactsApp {
             Files.write(contactsTxtPath, newContactList, StandardOpenOption.APPEND);
         }
 
+
+
+
+    }
+
+    public static void closeMenu() throws IOException {
+        System.out.println("Thanks for using Contact Manager, here is a list of your contacts:");
+        System.out.println();
+
+        printContacts();
+
+    }
+
+    private static void printContacts() throws IOException {
+        String directory = "./src/data";
+        String filename = "contacts.txt";
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename);
+        Path contactsTxtPath = Paths.get(directory, filename);
+
         List<String> printList = Files.readAllLines(contactsTxtPath);
         System.out.println("printList = " + printList);
-
-
     }
 
-    public static void viewContacts() {
-
-    }
 
     public static void main(String[] args) throws IOException {
+        System.out.println("Welcome to Contact Manager CLI");
+        System.out.println();
         openMenu();
 
-//        addContacts();
 
     }
 }
